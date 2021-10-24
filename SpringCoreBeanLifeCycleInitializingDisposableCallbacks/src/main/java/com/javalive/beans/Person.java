98a17009ab4dea6fbe0d313e478471704517dad8
abc <abc@xyz.com>
2021-10-24 17:35:37 +0530

    Initial commit.

---
+++ SpringCoreBeanLifeCycleInitializingDisposableCallbacks/src/main/java/com/javalive/beans/Person.java
@@ -0,0 +1,31 @@
+package com.javalive.beans;
+
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+/**
+ * @author JavaLive.com
+ *
+ */
+public class Person implements InitializingBean,DisposableBean{
+	 
+	   private String name;
+	   public Person()
+	   {
+	      System.out.println("Constructor of person bean is called !! ");
+	   }
+	   public void afterPropertiesSet() throws Exception // this is method of InitializingBean interface.
+	   {
+	   System.out.println("afterPropertiesSet method of person bean is called !! ");
+	   }
+	   public void destroy() throws Exception // this is method of DisposableBean interface.
+	   {
+	     System.out.println("destroy method of person bean is called !! ");
+	   }
+	   public String getName() {
+	    return name;
+	   }
+	   public void setName(String name) {
+	    this.name = name;
+	   }
+	}                
